6e988b4e0500b6fe520d81f8fafb8772dfee04db
cyla00 <ikhayam000@protonmail.com>
2022-02-18 14:08:42 +0100

    v1

--- server.js
+++ server.js
@@ -12,7 +12,6 @@ var cors = require('cors')
 
 var app = express()
 
-// app.use(express.static(__dirname))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 app.use(cookieParser())
